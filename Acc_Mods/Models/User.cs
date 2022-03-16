aed77b17d614a93689cb26b8d678a35299802ddf
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-03-16 11:08:54 +0400

    project uploaded
    project uploaded

---
+++ Acc_Mods/Models/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace UserClass.Models
+
+{
+    internal class User
+    {
+        private int _age;
+        public string UserName { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Cannot be negative!");
+                    return;
+                }
+                _age = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return Password;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    return;
+                }
+                if (value.Length < 8)
+                {
+                    Console.WriteLine("Not enough symbols");
+                    return;
+                }
+
+                if (IsexistsDigit(value) == false)
+                {
+                    return;
+                }
+                if (IsexistsUpperCase(value) == false)
+                {
+                    return;
+                }
+            }
+        }
+        private bool IsexistsUpperCase(string value)
+        {
+            foreach (var item in value)
+            {
+                if (Char.IsUpper(item))
+                {
+                    Console.WriteLine("True");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        private bool IsexistsDigit(string value)
+        {
+
+            foreach (var item in value)
+            {
+                if (Char.IsDigit(item))
+                {
+                    Console.WriteLine("True");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public User(string UserName, int age, string Password)
+        {
+            this.UserName = UserName;
+            this.Age = age;
+            this.Password = Password;
+        }
+    }
+
+}
---
+++ Acc_Mods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UserClass.Models;
+namespace UserClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Elon", -50, "muskelon");
+
+        }
+    }
+}
